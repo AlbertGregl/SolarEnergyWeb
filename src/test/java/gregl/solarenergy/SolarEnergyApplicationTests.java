68894b3c29d07a6e0b576805437f0e9b8a0d04d7
Albert Gregl <albert.gregl@gmail.com>
2024-03-03 17:18:27 +0100

    Initial commit

---
+++ src/test/java/gregl/solarenergy/SolarEnergyApplicationTests.java
@@ -0,0 +1,13 @@
+package gregl.solarenergy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolarEnergyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
